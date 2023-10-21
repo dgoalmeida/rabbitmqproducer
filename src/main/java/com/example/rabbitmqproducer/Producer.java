8abc8beaf96c42af0cb09287942714422a9c0837
Diego Almeida <diegoalmeida@MacBook-Pro-de-Diego.local>
2023-10-21 22:50:06 +0100

    Changing deployment version to: rabbitmq-producer:8.12

--- src/main/java/com/example/rabbitmqproducer/Producer.java
+++ src/main/java/com/example/rabbitmqproducer/Producer.java
@@ -1,16 +1,20 @@
 package com.example.rabbitmqproducer;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 @Component
 public class Producer {
 
+    private static final Logger LOG = LogManager.getLogger(Producer.class);
     @Autowired
     private RabbitTemplate rabbitTemplate;
 
     public String sendMessage(String message) {
+
+        LOG.info(message);
         rabbitTemplate.convertAndSend("default_exchange", "routing",
                 message);
         return "Message(" + message + ")" + " has been produced.";
